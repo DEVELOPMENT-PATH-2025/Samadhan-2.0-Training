19e0f7ca0574fa2ce737b3548e868b0c360aef32
Amritanshu_Tiwari_SDE <westerncourt4@gmail.com>
2026-05-07 21:47:41 +0530

    final changes

---
+++ ascii.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int i;
+    char ch;
+//    for(i=65;i<=100;i++){
+  //      printf("%d = %c\n",i,i);
+    //}
+    //return 0;
+    printf("Enter the character");
+    scanf("%c",&ch);
+    printf("%d",ch);
+    return 0;
+}
